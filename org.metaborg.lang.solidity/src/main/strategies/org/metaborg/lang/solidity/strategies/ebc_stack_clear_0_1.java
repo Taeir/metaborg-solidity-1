686bfd898c0c229ee2c7d00e5ba82c78975cb7f7
Taico Aerts <info@taico.nl>
2018-06-15 23:33:42 +0200

    Copy all ebc functions to solidity
    The imported functions cannot be found otherwise.

---
+++ org.metaborg.lang.solidity/src/main/strategies/org/metaborg/lang/solidity/strategies/ebc_stack_clear_0_1.java
@@ -0,0 +1,31 @@
+package org.metaborg.lang.solidity.strategies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.strategoxt.lang.Context;
+
+/**
+ * Clears the stack.
+ * 
+ * ebc-stack-clear(|stack) _ -> list(instruction)
+ */
+public class ebc_stack_clear_0_1 extends SafeStrategy {
+	public static final ebc_stack_clear_0_1 instance = new ebc_stack_clear_0_1();
+	
+	private ebc_stack_clear_0_1() {}
+	
+	@Override
+	public IStrategoTerm call(Context context, IStrategoTerm current, IStrategoTerm stackTerm) throws Exception {
+		if (!(stackTerm instanceof EBCStrategoStack)) return null;
+		
+		EBCStrategoStack stack = (EBCStrategoStack) stackTerm;
+		
+		int toPop = stack.size();
+		stack.clear();
+		List<IStrategoTerm> instructions = new ArrayList<>();
+		StackUtil.addNPops(context, instructions, toPop);
+		return context.getFactory().makeList(instructions);
+	}
+}
